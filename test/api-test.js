036ee1ea4c6eeb2c6329b794bce67aa3f61afa50
Nick Bottomley <nhbottomley@gmail.com>
2014-11-26 18:00:12 -0800

    should pass?

--- test/api-test.js
+++ test/api-test.js
@@ -11,6 +11,7 @@ chai.use(sinonChai);
 var spy = require("./nba-api-spy");
 var json = require("./get-json-stub");
 
+var noop = Function();
 
 var returnArg = function (a) { return a; };
 
@@ -30,88 +31,88 @@ api.__set__("getJSON", successSpy);
 
 describe(".playerProfile()", function () {
   it("should issue a request to the correct URL", function () {
-    api.playerProfile();
+    api.playerProfile(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/playerprofile").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.playerProfile({playerId: 1234});
+    api.playerProfile({playerId: 1234}, noop);
     successSpy.lastCalledWithOption("PlayerID", 1234).should.equal(true);
   });
 });
 
 describe(".playerInfo()", function () {
   it("should issue a request to the correct URL", function () {
-    api.playerInfo();
+    api.playerInfo(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/commonplayerinfo").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.playerInfo({playerId: 1});
+    api.playerInfo({playerId: 1}, noop);
     successSpy.lastCalledWithOption("PlayerID", 1).should.equal(true);
   });
 });
 
 describe(".playerSplits()", function () {
   it("should issue a request to the correct URL", function () {
-    api.playerSplits();
+    api.playerSplits(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/playerdashboardbygeneralsplits").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.playerSplits({playerId: 2});
+    api.playerSplits({playerId: 2}, noop);
     successSpy.lastCalledWithOption("PlayerID", 2).should.equal(true);
   });
 });
 
 describe(".playersInfo()", function () {
   it("should issue a request to the correct URL", function () {
-    api.playersInfo();
+    api.playersInfo(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/commonallplayers").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.playersInfo({season: "2013-14"});
+    api.playersInfo({season: "2013-14"}, noop);
     successSpy.lastCalledWithOption("Season", "2013-14").should.equal(true);
   });
 });
 
 describe(".teamStats()", function () {
   it("should issue a request to the correct URL", function () {
-    api.teamStats();
+    api.teamStats(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/leaguedashteamstats").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.teamStats({season: "2012-13"});
+    api.teamStats({season: "2012-13"}, noop);
     successSpy.lastCalledWithOption("Season", "2012-13").should.equal(true);
   });
 });
 
 describe(".teamSplits()", function () {
   it("should issue a request to the correct URL", function () {
-    api.teamSplits();
+    api.teamSplits(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/teamdashboardbygeneralsplits").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.teamSplits({season: "2011-12"});
+    api.teamSplits({season: "2011-12"}, noop);
     successSpy.lastCalledWithOption("Season", "2011-12").should.equal(true);
   });
 });
 
 describe(".teamYears()", function () {
   it("should issue a request to the correct URL", function () {
-    api.teamYears();
+    api.teamYears(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/commonteamyears").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.teamYears({leagueId: "00"});
+    api.teamYears({leagueId: "00"}, noop);
     successSpy.lastCalledWithOption("LeagueID", "00").should.equal(true);
   });
 });
 
 describe(".shots()", function () {
   it("should issue a request to the correct URL", function () {
-    api.shots();
+    api.shots(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/shotchartdetail").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.shots({playerId: 3});
+    api.shots({playerId: 3}, noop);
     successSpy.lastCalledWithOption("PlayerID", 3).should.equal(true);
   });
 });
@@ -119,77 +120,77 @@ describe(".shots()", function () {
 
 describe(".scoreboard()", function () {
   it("should issue a request to the correct URL", function () {
-    api.scoreboard();
+    api.scoreboard(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/scoreboard").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.scoreboard({gameDate: "12/25/2014"});
+    api.scoreboard({gameDate: "12/25/2014"}, noop);
     successSpy.lastCalledWithOption("GameDate", "12/25/2014").should.equal(true);
   });
 });
 
 describe(".playByPlay()", function () {
   it("should issue a request to the correct URL", function () {
-    api.playByPlay();
+    api.playByPlay(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/playbyplay").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.playByPlay({gameId: 1});
+    api.playByPlay({gameId: 1}, noop);
     successSpy.lastCalledWithOption("GameID", 1).should.equal(true);
   });
 });
 
 describe(".boxScoreScoring()", function () {
   it("should issue a request to the correct URL", function () {
-    api.boxScoreScoring();
+    api.boxScoreScoring(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/boxscorescoring").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.boxScoreScoring({gameId: 2});
+    api.boxScoreScoring({gameId: 2}, noop);
     successSpy.lastCalledWithOption("GameID", 2).should.equal(true);
   });
 });
 
 describe(".boxScoreUsage()", function () {
   it("should issue a request to the correct URL", function () {
-    api.boxScoreUsage();
+    api.boxScoreUsage(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/boxscoreusage").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.boxScoreUsage({gameId: 3});
+    api.boxScoreUsage({gameId: 3}, noop);
     successSpy.lastCalledWithOption("GameID", 3).should.equal(true);
   });
 });
 
 describe(".boxScoreMisc()", function () {
   it("should issue a request to the correct URL", function () {
-    api.boxScoreMisc();
+    api.boxScoreMisc(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/boxscoremisc").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.boxScoreMisc({gameId: 4});
+    api.boxScoreMisc({gameId: 4}, noop);
     successSpy.lastCalledWithOption("GameID", 4).should.equal(true);
   });
 });
 
 describe(".boxScoreAdvanced()", function () {
   it("should issue a request to the correct URL", function () {
-    api.boxScoreAdvanced();
+    api.boxScoreAdvanced(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/boxscoreadvanced").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.boxScoreAdvanced({gameId: 5});
+    api.boxScoreAdvanced({gameId: 5}, noop);
     successSpy.lastCalledWithOption("GameID", 5).should.equal(true);
   });
 });
 
 describe(".boxScoreFourFactors()", function () {
   it("should issue a request to the correct URL", function () {
-    api.boxScoreFourFactors();
+    api.boxScoreFourFactors(noop);
     successSpy.lastCalledWithUrl("http://stats.nba.com/stats/boxscorefourfactors").should.equal(true);
   });
   it("should issue a request with the correct params", function () {
-    api.boxScoreFourFactors({gameId: 6});
+    api.boxScoreFourFactors({gameId: 6}, noop);
     successSpy.lastCalledWithOption("GameID", 6).should.equal(true);
   });
 });
