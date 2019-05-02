a5338298b666ea9c8bbebdc5bfd3813d95b7c239
Nick Bottomley <nhbottomley@gmail.com>
2019-05-02 20:01:47 +0000

    move HTTP client to axios

--- src/get-json.js
+++ src/get-json.js
@@ -1,7 +1,7 @@
-const url = require("url");
 const template = require("nba-client-template");
+const axios = require("axios");
 
-const HEADERS = {
+const BASE_HEADERS = {
   "Accept-Encoding": "gzip, deflate",
   "Accept-Language": "en-US",
   Accept: "*/*",
@@ -12,48 +12,14 @@ const HEADERS = {
   Origin: "http://stats.nba.com",
 };
 
-function createUrlString (_url, query) {
-  const urlObj = url.parse(_url);
-  urlObj.query = query;
-  return urlObj.format();
+const fetcher = axios.create({
+  headers: BASE_HEADERS
+})
+
+function getJSON (url, params, { headers = {} } = {}) {
+  return fetcher.get(url, { params, headers })
+    .then(resp => resp.data)
 }
 
-function createGetJson () {
-  const fetch = require("node-fetch");
-
-  return function getJson (_url, query, _options = {}) {
-    const urlStr = createUrlString(_url, query);
-
-    const options = Object.assign({}, _options);
-    options.headers = Object.assign((options.headers || {}), HEADERS);
-
-    return fetch(urlStr, options)
-      .then(resp => {
-        if (resp.ok) return resp.json();
-
-        return resp.text().then(function (text) {
-          throw new Error(`${resp.status} ${resp.statusText} – ${text}`);
-        });
-      });
-  };
-}
-
-function createGetJsonp () {
-  const jsonp = require("jsonp");
-
-  return function getJsonp (_url, query, options = {}) {
-    return new Promise(function (resolve, reject) {
-      const urlStr = createUrlString(_url, query);
-
-      jsonp(urlStr, {timeout: options.timeout}, function (err, data) {
-        // for compatibility with timeouts from request module
-        if (err && err.message === "Timeout") err.code = "ETIMEDOUT";
-        if (err) return reject(err);
-        return resolve(data);
-      });
-    });
-  };
-}
-
-module.exports = typeof window === "undefined" ?
-  createGetJson() : createGetJsonp();
+module.exports = getJSON;
+exports.BASE_HEADERS = BASE_HEADERS;
